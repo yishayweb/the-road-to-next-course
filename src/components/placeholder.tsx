c6060511699a0627a1de424d49ee225bf7544e26
yishay-at-bay <95177000+yishay-at-bay@users.noreply.github.com>
2025-01-08 19:48:27 +0200

    updated npm registry

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -16,11 +16,11 @@ const Placeholder = ({
     <div className="flex-1 self-center flex flex-col items-center justify-center gap-y-2">
       {cloneElement(icon, {
         className: "w-16 h-16",
-      })}
+      } as { className: string })}
       <h2 className="text-lg text-center">{label}</h2>
       {cloneElement(button, {
         className: "h-10",
-      })}
+      } as { className: string })}
     </div>
   );
 };
--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -1,10 +1,9 @@
 "use client"
 
-import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
-
-import { cn } from "@/lib/utils"
+import * as React from "react"
 import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
@@ -128,14 +127,14 @@ AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName
 
 export {
   AlertDialog,
-  AlertDialogPortal,
-  AlertDialogOverlay,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
   AlertDialogAction,
   AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 }
