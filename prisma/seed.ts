2fd62709aca0a7a004583d3053293280de1bd5ce
yishay-at-bay <95177000+yishay-at-bay@users.noreply.github.com>
2025-04-18 22:30:01 +0300

    updated ticket item component no get auth

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -42,10 +42,17 @@ const tickets = [
   },
 ];
 
+const comments = [
+  { content: "First comment from DB." },
+  { content: "Second comment from DB." },
+  { content: "Third comment from DB." },
+];
+
 const seed = async () => {
   const t0 = performance.now();
   console.log("DB Seed: Started ...");
 
+  await prisma.comment.deleteMany();
   await prisma.user.deleteMany();
   await prisma.ticket.deleteMany();
 
@@ -58,10 +65,18 @@ const seed = async () => {
     })),
   });
 
-  await prisma.ticket.createMany({
+  const dbTickets = await prisma.ticket.createManyAndReturn({
     data: tickets.map((ticket) => ({
       ...ticket,
       userId: dbUsers[0].id,
+    })),
+  });
+
+  await prisma.comment.createMany({
+    data: comments.map((comment) => ({
+      ...comment,
+      ticketId: dbTickets[0].id,
+      userId: dbUsers[1].id,
     })),
   });
 
--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -2,6 +2,7 @@ import { Placeholder } from "@/components/placeholder";
 import { getTickets } from "../queries/get-tickets";
 import { ParsedSearchParams } from "../search-params";
 import { TicketItem } from "./ticket-item";
+import { TicketPagination } from "./ticket-pagination";
 import { TicketSearchInput } from "./ticket-search-input";
 import { TicketSortSelect } from "./ticket-sort-select";
 
@@ -11,7 +12,10 @@ type TicketListProps = {
 };
 
 const TicketList = async ({ userId, searchParams }: TicketListProps) => {
-  const tickets = await getTickets(userId, searchParams);
+  const { list: tickets, metadata: ticketMetadata } = await getTickets(
+    userId,
+    searchParams
+  );
 
   return (
     <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
@@ -43,6 +47,10 @@ const TicketList = async ({ userId, searchParams }: TicketListProps) => {
       ) : (
         <Placeholder label="No tickets found" />
       )}
+
+      <div className="w-full max-w-[420px]">
+        <TicketPagination paginatedTicketMetadata={ticketMetadata} />
+      </div>
     </div>
   );
 };
